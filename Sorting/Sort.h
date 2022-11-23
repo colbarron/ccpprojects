cd27dbe95a0101685c12a1258b9b5f7e32b1e552
Gary Mejia <gmejiama@ucsc.edu>
2022-11-23 01:18:46 -0800

    Added a couple function declarations in Sort.h, more to follow

--- Sorting/Sort.h
+++ Sorting/Sort.h
@@ -1,2 +1,17 @@
 #include "List.h"
 
+//O(n^2) Sorting Algorithms
+
+/*Takes in a List A and performs an Insert Sort and overwrites
+ the contents of List A*/
+void InsertSort(List A);
+
+/*Takes in a List A and performs a Selection Sort and overwrites
+ the contents of List A*/
+void SelectionSort(List A);
+
+//O(nlog(n)) Sorting Algorithms
+
+/*Takes in a List A and performs a Merge Sort and overwrites the 
+ contents of List A */
+void MergeSort(List A);
